028e101a34c2dfc71abd8c06edeb0883cd10a3d2
claramelo <clarad3oliveira@gmail.com>
2026-05-24 15:37:38 -0300

    feat: esqueleto do main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Jogador.hpp"
+#include "Tiro.hpp"
+#include "TiroCapivara.hpp"
+#include "TiroTucano.hpp"
+#include "Onca.hpp"
+#include "Capivara.hpp"
+#include "Tucano.hpp"
+#include "Boss.hpp"
+#include "Pipa.hpp"
+#include "Pedra.hpp"
+#include "Guarana.hpp"
+
+int main() {
+    Jogador Heliconia;
+
+    std::cout << "Ecos de Aranãmi\n\n";
+    std::cout << Heliconia;
+
+    return 0;
+}
