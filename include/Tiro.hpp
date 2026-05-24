d0675d5057076905e316759e4a8706c60a06e5aa
Matheus Reis <mrrs@cin.ufpe.br>
2026-05-24 13:40:33 -0300

    proteção do método ricochetear( ) do tiro.

--- include/Tiro.hpp
+++ include/Tiro.hpp
@@ -4,12 +4,12 @@
 #include "Projetil.hpp"
 
 class Tiro : public Projetil{
+    protected:
+        void ricochetear(float parede_esquerda, float parede_direita); // verificar se o tiro bateu na parede e ricochetear
     public:
         Tiro(float pos_x, float pos_y, int direcao);
         ~Tiro();
-
         void update() override;
-        void ricochetear(float parede_esquerda, float parede_direita); // verificar se o tiro bateu na parede e ricochetear
         void desativar() override;
 };
 
